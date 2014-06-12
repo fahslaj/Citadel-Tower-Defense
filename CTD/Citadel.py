94efc99e9cb460deeeb31872848523c4c7ac7962
fahslaj <fahslaj@rose-hulman.edu>
2014-06-12 13:22:54 -0400

    Added Citadel Base class

---
+++ CTD/Citadel.py
@@ -0,0 +1,18 @@
+'''
+Created on Jun 12, 2014
+
+@author: fahslaj
+'''
+
+''' The base class for a Citadel tower '''
+class Citadel(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
